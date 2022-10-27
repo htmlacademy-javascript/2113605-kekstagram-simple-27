2847e05079af6034dbfe745fd46ae2d1d8044b7f
Alex48Yakunin <39828189+Alex48Yakunin@users.noreply.github.com>
2022-10-27 17:58:14 +0300

    + модуль работы с формой

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 import {renderPicture} from './render-picture.js';
+import {onEditModalControl, manageImageSize, applyEffectOnImage} from './form.js';
 
 renderPicture();
+onEditModalControl();
+manageImageSize();
+applyEffectOnImage();
+
